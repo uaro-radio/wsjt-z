64f54fd8fad80875bccf4302d1e38eb32c2774ff
Kelly <kellykeeton@yahoo.com>
2026-06-12 16:39:25 -0700

    patch-auto-band-hop
    as referenced https://github.com/sq9fve/wsjt-z/issues/74
    
    patch-info-tray
    
    band hop info bar fixes

--- widgets/mainwindow.cpp
+++ widgets/mainwindow.cpp
@@ -3573,16 +3573,16 @@ void MainWindow::update_mode_switch_status_label ()
               int bh_remaining = 0;
               if (ui->cbAutoCall->isChecked ())
                 {
-                  // Updated audo-freq behavior hops at AutoCQ -> AutoCall when
-                  // auto mode switch is enabled.
+                  // With auto mode switch enabled, the next band hop occurs at
+                  // the end of the current AutoCall cycle.
                   bh_remaining = auto_call_left;
-                  if (ui->cb_autoModeSwitch->isChecked ()) {
-                    bh_remaining += auto_cq_total;
-                  }
                 }
               else if (ui->cbAutoCQ->isChecked ())
                 {
                   bh_remaining = auto_cq_left;
+                  if (ui->cb_autoModeSwitch->isChecked ()) {
+                    bh_remaining += auto_call_total;
+                  }
                 }
 
               if (bh_remaining > 0)
@@ -15104,6 +15104,9 @@ void MainWindow::ZProcess ()
                             m_autoModeSwitch = true;
                             ui->cbAutoCall->setChecked(false);
                             ui->cbAutoCQ->setChecked(true);
+                            // With auto mode switch enabled, hop at the
+                            // AutoCall -> AutoCQ boundary.
+                            if (ui->cb_bandHopper->isChecked()) toggleBands();
                             if (m_smartModeSwitch) {
                               ui->cbHoldTxFreq->setChecked(true);
                               if (m_config.autoTXFreq()) {
@@ -15164,9 +15167,6 @@ void MainWindow::ZProcess ()
                                 ui->cbHoldTxFreq->setChecked(false);
                               }
                               m_autoModeSwitch = false;
-                              // With auto mode switch enabled, hop only at the
-                              // AutoCQ -> AutoCall boundary.
-                              if (ui->cb_bandHopper->isChecked()) toggleBands();
                               if (m_zdebug) log("ZProcess: Switched to AutoCall");
                           } else {
                               toggleBands();
